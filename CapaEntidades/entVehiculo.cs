02ae780154f4a88c770a5b6f85197d99ad39d63d
Nilo Salvador León <nsalvadorl@ijamvirtual.edu.pe>
2024-11-25 21:24:48 -0500

    Versión 2 - Nilo Salvador

---
+++ CapaEntidades/entVehiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class entVehiculo
+    {
+        public int idVehiculo { get; set; }
+        public String placa { get; set; }
+        public Boolean estado { get; set; }
+    }
+}
