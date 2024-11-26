f024ec4cbf989733d027183747af2e5b1c1768dd
NiloSalvadorLeón <NiloSalvadorLen@PC-ING-NILO-S>
2024-11-25 21:18:45 -0500

    Agregar archivos de proyecto.

---
+++ CapaEntidades/entUsuario.cs
@@ -0,0 +1,14 @@
+﻿namespace CapaEntidades
+{
+    public class entUsuario
+    {
+        public int idUsuario { get; set; }
+        public String nombres { get; set; }
+        public String apellidos { get; set; }
+        public String tipoUsuario { get; set; } //(Empleado, Estudiante, Visitante)
+        public int dni { get; set; }
+        public int celular { get; set; }
+        public Boolean estado { get; set; } //Habilitado o Inhabilitado
+
+    }
+}
